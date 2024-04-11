a7ba9572929680d932f46edc6ecdaeacc32e2f88
LeeTaeHeon <forever296@naver.com>
2024-04-11 22:36:48 +0900

    design: 번역하기 스크린 UI 구성 완료

--- screen/TranslationScreen/TranslationScreen.js
+++ screen/TranslationScreen/TranslationScreen.js
@@ -1,14 +1,41 @@
 // Inside TranslationScreen.js
 
 import React from "react";
-import { View } from "react-native";
+import { View, TextInput, TouchableOpacity } from "react-native";
 import AppText from "../../components/common/AppText";
 import { styles } from "./TranslationScreenStyle";
+import { AntDesign } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 
 const TranslationScreen = () => {
   return (
     <View style={styles.container}>
-      <AppText>번역하기</AppText>
+      <TextInput style={styles.input} placeholder="텍스트 입력" />
+      <View style={styles.bottom}>
+        <View style={styles.translatedLanguage}>
+          <AppText style={styles.language}>영어</AppText>
+          <AntDesign name="arrowright" size={24} color="black" />
+          <AppText style={styles.language}>한국어</AppText>
+        </View>
+        <View style={styles.transBtn}>
+          <View style={styles.side}>
+            <TouchableOpacity style={styles.sideBtn}>
+              <FontAwesome name="photo" size={24} color="black" />
+            </TouchableOpacity>
+            <AppText style={styles.sideText}>사진 불러오기</AppText>
+          </View>
+
+          <TouchableOpacity style={styles.centerBtn}>
+            <FontAwesome name="microphone" size={24} color="white" />
+          </TouchableOpacity>
+          <View style={styles.side}>
+            <TouchableOpacity style={styles.sideBtn}>
+              <AntDesign name="camera" size={24} color="black" />
+            </TouchableOpacity>
+            <AppText style={styles.sideText}>사진 찍기</AppText>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
--- screen/TranslationScreen/TranslationScreenStyle.js
+++ screen/TranslationScreen/TranslationScreenStyle.js
@@ -4,6 +4,67 @@ import theme from "../../config/theme";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#EBE7FF",
+  },
+  input: {
+    width: theme.screenWidth,
+    height: theme.screenHeight - 250,
+    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 20,
     backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    fontSize: 18,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  translatedLanguage: {
+    flexDirection: "row",
+    marginTop: 10,
+    gap: 20,
+    marginLeft: "auto",
+    marginRight: "auto",
+    alignItems: "center",
+  },
+  language: {
+    backgroundColor: "#fff",
+    paddingTop: 5,
+    paddingBottom: 5,
+    width: 130,
+    borderRadius: 10,
+    textAlign: "center",
+    fontWeight: 600,
+  },
+  transBtn: {
+    flexDirection: "row",
+    gap: 40,
+    ...theme.centerStyle,
+  },
+  sideBtn: {
+    backgroundColor: "#CAC1FF",
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    ...theme.centerStyle,
+  },
+  centerBtn: {
+    backgroundColor: "#5337FF",
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    ...theme.centerStyle,
+  },
+  sideText: {
+    fontWeight: "600",
+    fontSize: 12,
+  },
+  side: {
+    ...theme.centerStyle,
+    marginTop: 40,
   },
 });
