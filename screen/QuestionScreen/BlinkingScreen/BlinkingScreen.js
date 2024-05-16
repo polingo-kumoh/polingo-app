1d5292411a72a61ac4a978293aae24d850d03bdc
LeeTaeHeon <forever296@naver.com>
2024-05-16 18:24:37 +0900

    design: 깜빡이 스크린 문제 넘어가는 기능 개발

--- screen/QuestionScreen/BlinkingScreen/BlinkingScreen.js
+++ screen/QuestionScreen/BlinkingScreen/BlinkingScreen.js
@@ -1,15 +1,80 @@
-// Inside BlinkingScreen.js
-
-import React from "react";
-import { View } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import { View, TouchableOpacity } from "react-native";
 import AppText from "../../../components/common/AppText";
+import { styles } from "./BlinkingScreenStyle";
+import { useAuth } from "../../../config/AuthContext";
+import { useQuizData } from "./../../../hooks/useQuizData";
+import QuizQuestion from "./../../../components/component/QuizQuestion/QuizQuestion";
+import { MaterialIcons, Feather, AntDesign } from "@expo/vector-icons";
 
 const BlinkingScreen = ({ navigation, route }) => {
   const { defaultCategoryId } = route.params;
-  console.log(defaultCategoryId);
+  const { token } = useAuth();
+  const {
+    data: quizData,
+    isLoading,
+    isError,
+    error,
+    refetch,
+  } = useQuizData(token, defaultCategoryId);
+
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const isPausedRef = useRef(false);
+  const progress = quizData
+    ? ((currentQuestionIndex + 1) / quizData?.count) * 100
+    : 0;
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitle: () => (
+        <View style={styles.header}>
+          <AppText style={styles.quizCount}>
+            {currentQuestionIndex + 1}/{quizData?.count}
+          </AppText>
+        </View>
+      ),
+    });
+  }, [navigation, currentQuestionIndex, quizData]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!isPausedRef.current) {
+        setCurrentQuestionIndex((prevIndex) =>
+          prevIndex < quizData?.quizes.length - 1 ? prevIndex + 1 : 0
+        );
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [quizData?.quizes.length]); // quizData의 질문 개수에만 의존
+
+  const handlePause = () => {
+    isPausedRef.current = !isPausedRef.current;
+  };
   return (
-    <View>
-      <AppText>BlinkingScreen</AppText>
+    <View style={styles.container}>
+      <View style={styles.progressBarContainer}>
+        <View style={[styles.progressBar, { width: `${progress}%` }]} />
+      </View>
+      <QuizQuestion
+        question={quizData?.quizes[currentQuestionIndex]?.question}
+      />
+      <View style={styles.iconsView}>
+        {isPausedRef.current ? (
+          <TouchableOpacity onPress={handlePause}>
+            <AntDesign name="playcircleo" size={40} color="black" />
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity onPress={handlePause}>
+            <Feather name="pause-circle" size={40} color="black" />
+          </TouchableOpacity>
+        )}
+
+        <TouchableOpacity
+          onPress={() => setCurrentQuestionIndex(currentQuestionIndex + 1)}
+        >
+          <MaterialIcons name="double-arrow" size={40} color="black" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
--- screen/QuestionScreen/BlinkingScreen/BlinkingScreenStyle.js
+++ screen/QuestionScreen/BlinkingScreen/BlinkingScreenStyle.js
@@ -1,4 +1,34 @@
 import { StyleSheet } from "react-native";
 import theme from "../../../config/theme";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 10,
+    ...theme.centerStyle,
+  },
+  progressBarContainer: {
+    position: "absolute", // 상단에 고정
+    top: 0, // 상단에서 0의 위치에 있음
+    left: 0,
+    right: 0,
+    height: 2,
+    width: "100%",
+    backgroundColor: "grey",
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  progressBar: {
+    height: "100%",
+    backgroundColor: "blue",
+  },
+  quizCount: {
+    fontWeight: "600",
+    fontSize: 18,
+  },
+  iconsView: {
+    flexDirection: "row",
+    gap: 20,
+  },
+});
