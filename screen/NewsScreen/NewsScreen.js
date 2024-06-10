2fa7ed3c0f7e0828c241a82515881d034dd3b56d
LeeTaeHeon <forever296@naver.com>
2024-06-10 17:56:26 +0900

    feat: 뉴스 스크린 최적화

--- screen/NewsScreen/NewsScreen.js
+++ screen/NewsScreen/NewsScreen.js
@@ -43,6 +43,8 @@ const NewsScreen = ({ navigation }) => {
   const {
     data: scrapData,
     isLoading: isScrapDataLoading,
+    isError: isScrapError,
+    error: scrapError,
     refetch: refetchScrapData,
   } = useScrapData(
     token,
@@ -59,6 +61,9 @@ const NewsScreen = ({ navigation }) => {
       setNewsItems((prev) => [...prev, ...uniqueNews]);
       setHasMoreNews(!newsData.last);
     }
+  }, [newsData]);
+
+  useEffect(() => {
     if (scrapData && scrapData.content) {
       const uniqueScrap = scrapData.content.filter(
         (sd) => !scrapItems.find((item) => item.id === sd.id)
@@ -66,141 +71,150 @@ const NewsScreen = ({ navigation }) => {
       setScrapItems((prev) => [...prev, ...uniqueScrap]);
       setHasMoreScrap(!scrapData.last);
     }
-  }, [newsData, scrapData]);
-
-  useEffect(() => {
-    if (refreshing) {
-      refetchNewsData().then(() =>
-        refetchScrapData().then(() => setRefreshing(false))
-      );
-    }
-  }, [refreshing]);
+  }, [scrapData]);
 
   const onRefresh = async () => {
     setRefreshing(true);
     setNewsPage(0);
     setScrapPage(0);
-    await Promise.all([refetchNewsData(), refetchScrapData()]);
-    setRefreshing(false);
+    try {
+      await Promise.all([refetchNewsData(), refetchScrapData()]);
+    } finally {
+      setRefreshing(false);
+    }
   };
 
   const loadMoreNews = () => {
-    if (hasMoreNews) {
+    if (hasMoreNews && !isNewsDataLoading) {
       setNewsPage((prev) => prev + 1);
     }
   };
 
   const loadMoreScrap = () => {
-    if (hasMoreScrap) {
+    if (hasMoreScrap && !isScrapDataLoading) {
       setScrapPage((prev) => prev + 1);
     }
   };
+
+  const renderNewsItem = ({ item }) => (
+    <TouchableOpacity
+      style={styles.newsContents}
+      onPress={() =>
+        navigation.navigate("NewsDetailScreen", {
+          idx: item.id,
+          publishData: item.publish_date.split("T")[0],
+        })
+      }
+    >
+      <Image source={{ uri: item.image_url }} style={styles.newsImage} />
+      <View style={styles.newsTextContainer}>
+        <AppText
+          style={styles.newsContentTitle}
+          numberOfLines={1}
+          ellipsizeMode="tail"
+        >
+          {item.title}
+        </AppText>
+        <AppText
+          style={styles.newsContentsContent}
+          numberOfLines={3}
+          ellipsizeMode="tail"
+        >
+          {item.first_sentence}
+        </AppText>
+      </View>
+      <AppText style={styles.publishDate}>
+        {item.publish_date.split("T")[0]}
+      </AppText>
+    </TouchableOpacity>
+  );
+
+  const renderScrapItem = ({ item }) => (
+    <TouchableOpacity
+      style={styles.newsContents}
+      onPress={() =>
+        navigation.navigate("NewsDetailScreen", {
+          idx: item.id,
+          publishData: item.publish_date.split("T")[0],
+        })
+      }
+    >
+      <Image source={{ uri: item.image_url }} style={styles.newsImage} />
+      <View style={styles.newsTextContainer}>
+        <AppText
+          style={styles.newsContentTitle}
+          numberOfLines={1}
+          ellipsizeMode="tail"
+        >
+          {item.title}
+        </AppText>
+        <AppText
+          style={styles.newsContentsContent}
+          numberOfLines={3}
+          ellipsizeMode="tail"
+        >
+          {item.first_sentence}
+        </AppText>
+      </View>
+      <AppText style={styles.publishDate}>
+        {item.publish_date.split("T")[0]}
+      </AppText>
+    </TouchableOpacity>
+  );
+
   return (
     <View style={styles.container}>
       {/* Scrap Section */}
       <View style={styles.newsContainer}>
         <AppText style={styles.newsTitle}>Scrap</AppText>
-        <FlatList
-          data={scrapItems}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          keyExtractor={(item) => String(item.id)}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              style={styles.newsContents}
-              onPress={() =>
-                navigation.navigate("NewsDetailScreen", {
-                  idx: item.id,
-                  publishData: item.publish_date.split("T")[0],
-                })
-              }
-            >
-              <Image
-                source={{ uri: item.image_url }}
-                style={styles.newsImage}
-              />
-              <View style={styles.newsTextContainer}>
-                <AppText
-                  style={styles.newsContentTitle}
-                  numberOfLines={1}
-                  ellipsizeMode="tail"
-                >
-                  {item.title}
-                </AppText>
-                <AppText
-                  style={styles.newsContentsContent}
-                  numberOfLines={3}
-                  ellipsizeMode="tail"
-                >
-                  {item.first_sentence}
-                </AppText>
-              </View>
-              <AppText style={styles.publishDate}>
-                {item.publish_date.split("T")[0]}
-              </AppText>
-            </TouchableOpacity>
-          )}
-          ListEmptyComponent={() => (
-            <AppText style={styles.emptyMessage}>No scrap news yet!</AppText>
-          )}
-          refreshing={refreshing}
-          onRefresh={onRefresh}
-          onEndReached={loadMoreScrap}
-          onEndReachedThreshold={0.5}
-        />
+        {isScrapDataLoading && scrapPage === 0 ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <FlatList
+            data={scrapItems}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={(item) => String(item.id)}
+            renderItem={renderScrapItem}
+            ListEmptyComponent={() => (
+              <AppText style={styles.emptyMessage}>No scrap news yet!</AppText>
+            )}
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            onEndReached={loadMoreScrap}
+            onEndReachedThreshold={0.5}
+          />
+        )}
       </View>
       {/* News Section */}
       <View style={styles.newsContainer}>
         <AppText style={styles.newsTitle}>News</AppText>
-        <FlatList
-          data={newsItems}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          keyExtractor={(item) => String(item.id)}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              style={styles.newsContents}
-              onPress={() =>
-                navigation.navigate("NewsDetailScreen", {
-                  idx: item.id,
-                  publishData: item.publish_date.split("T")[0],
-                })
-              }
-            >
-              <Image
-                source={{ uri: item.image_url }}
-                style={styles.newsImage}
-              />
-              <View style={styles.newsTextContainer}>
-                <AppText
-                  style={styles.newsContentTitle}
-                  numberOfLines={1}
-                  ellipsizeMode="tail"
-                >
-                  {item.title}
-                </AppText>
-                <AppText
-                  style={styles.newsContentsContent}
-                  numberOfLines={3}
-                  ellipsizeMode="tail"
-                >
-                  {item.first_sentence}
-                </AppText>
-              </View>
-              <AppText style={styles.publishDate}>
-                {item.publish_date.split("T")[0]}
-              </AppText>
-            </TouchableOpacity>
-          )}
-          ListEmptyComponent={() => (
-            <AppText style={styles.emptyMessage}>No news yet!</AppText>
-          )}
-          refreshing={refreshing}
-          onRefresh={onRefresh}
-          onEndReached={loadMoreNews}
-          onEndReachedThreshold={0.5}
-        />
+        {isNewsDataLoading && newsPage === 0 ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <FlatList
+            data={newsItems}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={(item) => String(item.id)}
+            renderItem={renderNewsItem}
+            ListEmptyComponent={() => (
+              <AppText style={styles.emptyMessage}>No news yet!</AppText>
+            )}
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            onEndReached={loadMoreNews}
+            onEndReachedThreshold={0.5}
+          />
+        )}
       </View>
+      {(isNewsError || isScrapError) && (
+        <View style={styles.errorContainer}>
+          <AppText style={styles.errorMessage}>
+            {newsError?.message || scrapError?.message}
+          </AppText>
+        </View>
+      )}
     </View>
   );
 };
